905b682a68bdcc4ca8790db7d2cf83946daef507
Rusty Mina <rusty.mina@eee.upd.edu.ph>
2017-06-03 23:23:07 +0800

    Added label_reader.py

--- preprocess_scripts/labeler.py
+++ preprocess_scripts/labeler.py
@@ -10,6 +10,6 @@ with open("ytrain.txt", 'w', encoding='utf-8') as fd:
         n = n + 1
 
     n = 0
-    while n < POS_LABELS
+    while n < POS_LABELS:
         print("1", file=fd)
         n = n + 1 
